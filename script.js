96639102d41a62151ac9dc9edde99376791012a7
Henri Tamm <henri.tamm@outlook.com>
2021-08-19 15:38:08 +0300

    time and style changes

--- script.js
+++ script.js
@@ -2,17 +2,17 @@
 const startButton = document.getElementById("start-button");
 const resetButton = document.getElementById("reset-button");
 
-const red = document.getElementById("red");
 const green = document.getElementById("green");
-const yellow = document.getElementById("yellow");
+const blue = document.getElementById("blue");
+const red = document.getElementById("red");
 let redTimeOut;
 let greenTimeOut;
-let yellowTimeOut;
+let blueTimeOut;
 let resetTimeOut;
 
 function reset() {
     green.style.background = "black";
-    yellow.style.background = "black";
+    blue.style.background = "black";
     red.style.background = "black";
     startButton.disabled = false;
     resetButton.disabled = true;
@@ -24,28 +24,28 @@ startButton.addEventListener("click", function () {
     // kohtadele
     greenTimeOut = setTimeout(function () {
         green.style.background = "green";
-    }, 1000);
+    }, 5000);
 
     // valmis
-    yellowTimeOut = setTimeout(function () {
-        yellow.style.background = "yellow";
-    }, 2000);
+    blueTimeOut = setTimeout(function () {
+        blue.style.background = "blue";
+    }, 17000);
 
     // start
     redTimeOut = setTimeout(function () {
         red.style.background = "red";
-    }, 3000);
+    }, 19000);
 
     // reset
     resetTimeOut = setTimeout(function () {
         reset();
-    }, 4000);
+    }, 20000);
 }, false);
 
 resetButton.addEventListener("click", function () {
     reset();
     clearTimeout(greenTimeOut);
-    clearTimeout(yellowTimeOut);
+    clearTimeout(blueTimeOut);
     clearTimeout(redTimeOut);
     clearTimeout(resetTimeOut);
 }, false);
